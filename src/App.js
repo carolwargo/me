c731f56357d4fdce903a9ce2d7bfe43a6fb93256
Carol Wargo <Carolwargo@gmail.com>
2024-03-11 17:24:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-//import Home from "./views/Home";
 import Home from "./views/Home/Home";
 import Blog from "./views/Blog/Blog";
 import Restaurant from "./views/Restaurant/Restaurant"; 
@@ -10,16 +8,13 @@ import Resume from "./views/Resume/Resume";
 import About from "./views/About/About";
 import Contact from './views/Contact/Contact';
 import Portfolio from './views/Portfolio/Portfolio';
-import Test from "./views/Test/Test";
-//import Navbar from "./components/Navbar/Navbar";
-//<BrowserRouter basename="/react-portfolio" >
+
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <BrowserRouter basename="/me"> 
-   
+    <BrowserRouter basename="/me" >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
@@ -27,13 +22,13 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/test" element={<Test />} />
+     
           <Route path="/blog" element={<Blog />} />
           <Route path="/restaurant" element={<Restaurant />} />
         </Routes>
  
       </BrowserRouter>
-    </div>
+  
   );
 }
 
