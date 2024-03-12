490aa97e63707c8308030b5545f4a397c1a78acb
Carol Wargo <Carolwargo@gmail.com>
2024-03-12 13:30:30 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="me">
         <Routes>
         <Route path="/" element={<Home />}/>      
         </Routes>
